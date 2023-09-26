9d271999b50de508b84b8e1a9a4156a5e94b3906
wwitchq <viola03d@mail.ru>
2023-09-26 14:28:56 +0300

    jupyter

---
+++ main.py
@@ -0,0 +1,36 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+"""
+Из набора данных Titanic.csv выведите:
+количество пассажиров,-891
+средний возраст пассажира,-29.69911764705882
+среднуюю стоимость билета,--32.204207968574636
+Мах и мин цена билета-0.0 и 512.3292
+пассажира с максимальной ценой билета,- 258                     
+ Ward, Miss. Anna
+679    Cardeza, Mr. Thomas Drake Martinez
+737                Lesurer, Mr. Gustave J
+Name: Name, dtype: object
+средний возраст мужчин и женщин по отдельности,27.915708812260537 и 30.72664459161148
+средний процент выживаемости мужчин и женщин,-0.122334455667789 и 0.2615039281705948
+среднюю стоимость билета в зависимости от класса проживания,-59.95414404761905
+13.276029870129872
+27.079811801242233
+количество билетов по классам проживания,-644/77/168
+количество выживших пассажиров-342
+"""
+
